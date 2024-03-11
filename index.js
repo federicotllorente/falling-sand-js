990184af842e0f756a473f5c9bc1093c2857412c
Federico Tejedor Llorente <federicotllorente@gmail.com>
2024-03-11 20:39:06 +0100

    Now with colors!

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const DRAWING_PROB = 0.3
 let grid = []
 let int, canvas
 
+let hueValue = 200
+
 function create2DArray(cols, rows) {
   let arr = new Array(cols)
 
@@ -43,7 +45,7 @@ function loop() {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       const cell = grid[i][j]
-      ctx.fillStyle = `rgb(${cell * 255}, ${cell * 255}, ${cell * 255})`
+      ctx.fillStyle = `hsl(${cell}, 100%, ${cell > 0 ? 50 : 0}%)`
       ctx.fillRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE)
     }
   }
@@ -53,7 +55,7 @@ function loop() {
     for (let j = 0; j < nextGrid[i].length; j++) {
       const cell = grid[i][j]
 
-      if (cell === 1) {
+      if (cell > 0) {
         let cellBelow = grid[i][j + 1]
         let cellBelowA, cellBelowB
 
@@ -68,13 +70,13 @@ function loop() {
         }
 
         if (cellBelow === 0) {
-          nextGrid[i][j + 1] = 1
+          nextGrid[i][j + 1] = cell
         } else if (cellBelowA === 0) {
-          nextGrid[i - dir][j + 1] = 1
+          nextGrid[i - dir][j + 1] = cell
         } else if (cellBelowB === 0) {
-          nextGrid[i + dir][j + 1] = 1
+          nextGrid[i + dir][j + 1] = cell
         } else {
-          nextGrid[i][j] = 1
+          nextGrid[i][j] = cell
         }
       }
     }
@@ -126,11 +128,14 @@ function drawCellFromMouseCoordinates(event) {
         cellY = cellYbase + j
   
         if (cellX >= 0 && cellX <= GRID_COLS - 1 && cellY >= 0 && cellY <= GRID_ROWS - 1) {
-          grid[cellX][cellY] = 1
+          grid[cellX][cellY] = hueValue
         }
       }
     }
   }
+
+  hueValue += 0.3
+  if (hueValue > 360) hueValue = 1
 }
 
 // Start/pause/resume searching when the spacebar is tapped
